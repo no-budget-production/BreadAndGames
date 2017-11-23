5511c8eff0255697e6a7d8d81ec5b6771faeaaca
ibimsderalex <alexander.buechler25@gmail.com>
2017-11-24 00:54:45 +0100

    groundwork Enemy.AI
    added NavMeshAgent and Bullet Collision

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,16 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour {
+    [RequireComponent(typeof(NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+    NavMeshAgent pathfinder;
+    Transform target;
 
-	// Use this for initialization
-	void Start () {
-		
+    void Start()
+    {
+        pathfinder = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        StartCoroutine(UpdatePath());
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void Update()
+    {
+        pathfinder.SetDestination(target.position);
+    }
+
+    IEnumerator UpdatePath()
+    {
+        float refreshRate = 0.25f;
+
+        while (target != null)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
+            pathfinder.SetDestination(targetPosition);
+            yield return new WaitForSeconds(refreshRate);
+        }
+    }
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public LayerMask collisionMask;
 
     float speed = 10;
     float lifetime = 2;
@@ -18,8 +19,28 @@ public class Projectile : MonoBehaviour
 
 	void Update ()
     {
+        float moveDistance = speed * Time.deltaTime;
+        CheckCollisions(moveDistance);
+
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
 	}
+
+    void CheckCollisions(float moveDistance)
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide))
+        {
+            OnHitObject(hit);
+        }
+    }
+
+    void OnHitObject(RaycastHit hit)
+    {
+        Debug.Log(hit.collider.gameObject.name);
+        GameObject.Destroy(gameObject);
+    }
 
     IEnumerator Fade()
     {
