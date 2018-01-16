0612f5dad07dd448a162340fe8f87fc7eeac9f8b
Kniesel-PC\Kniesel <clemens.haeslera@gmail.com>
2018-01-16 09:15:19 +0100

    - Grid Scene added in Scenes - added (own, deleteable) prefab Tower - added (own, deleteable) materials - created a folder for "Shaders" - added (own, deleteable) shaders - added scripts in scripts ___________________________
    Still ongoing
    - Controls while building <- changeable input manager needed

---
+++ Assets/Scripts/GridGizmos.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGizmos : MonoBehaviour
+{
+    ////////Variables////////
+
+    [SerializeField]
+    private float size = 0.5f;
+
+    public GameObject grid_Visual;
+
+    
+
+    ////////Functions////////
+
+    public Vector3 GetNearestPointOnGrid(Vector3 position)
+    {
+        float posX = grid_Visual.transform.position.x;
+        //float posY = grid_Visual.transform.position.y;
+        float posZ = grid_Visual.transform.position.z;
+
+        float xCount = ((posX + 0.5f) - 5);
+        float yCount = position.y;
+        float zCount = ((posZ + 0.5f) - 5);
+
+        Vector3 result = new Vector3(
+            (float)xCount + (position.x / 1f),
+            (float)yCount,
+            (float)zCount + (position.z / 1f));
+
+        
+
+        return result;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int k = 0; k < 10; k++)
+            {
+                var point = GetNearestPointOnGrid(new Vector3(i, 0f, k));
+                Gizmos.DrawSphere(point, 0.1f);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Towerplacer.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Towerplacer : MonoBehaviour
+{
+    ////////Variables////////
+
+    public GameObject grid;
+    public GameObject highlightedGridField;
+    public GameObject tower;
+    public Vector3 buildposition;
+    public Material outline;
+    public Material basematerial;
+
+    private bool buildmode = false;
+    private bool chosen = false;
+    private bool rotate = false;
+
+    float hgfPosx;
+    float hgfPosy;
+    float hgfPosz;
+
+
+    ////////Functions////////
+
+    
+
+    private void Update()
+    {
+        BuildmodeON();
+        InBuildmodeMovement();
+        ChooseTower();
+        RotateTower();
+        PlaceTower();
+    }
+
+    void BuildmodeON()
+    {
+        if (!buildmode && Input.GetKeyDown(KeyCode.JoystickButton3))
+        {
+            buildmode = true;
+            grid.SetActive(true);
+        }
+    }
+
+
+    void InBuildmodeMovement()
+    {
+        if (buildmode)
+        {
+            if (Input.GetAxis("Horizontal") == -1)
+            {
+                Vector3 buildposition = new Vector3(hgfPosx - 1, hgfPosy, hgfPosz);
+                highlightedGridField.transform.Translate(buildposition);
+            }
+
+            if (Input.GetAxis("Horizontal") == 1)
+            {
+                Vector3 buildposition = new Vector3(hgfPosx + 1, hgfPosy, hgfPosz);
+                highlightedGridField.transform.Translate(buildposition);
+            }
+
+            if (Input.GetAxis("Vertical") == -1)
+            {
+                Vector3 buildposition = new Vector3(hgfPosx, hgfPosy, hgfPosz - 1);
+                highlightedGridField.transform.Translate(buildposition);
+            }
+
+            if (Input.GetAxis("Vertical") == 1)
+            {
+                Vector3 buildposition = new Vector3(hgfPosx, hgfPosy, hgfPosz + 1);
+                highlightedGridField.transform.Translate(buildposition);
+            }
+
+            //if (Input.GetKeyDown(KeyCode.A))
+            //{
+            //    Vector3 buildposition = new Vector3(hgfPosx - 1, hgfPosy, hgfPosz);
+            //    highlightedGridField.transform.Translate(buildposition);
+            //}
+
+            //if (Input.GetKeyDown(KeyCode.D))
+            //{
+            //    Vector3 buildposition = new Vector3(hgfPosx + 1, hgfPosy, hgfPosz);
+            //    highlightedGridField.transform.Translate(buildposition);
+            //}
+
+            //if (Input.GetKeyDown(KeyCode.W))
+            //{
+            //    Vector3 buildposition = new Vector3(hgfPosx, hgfPosy, hgfPosz + 1);
+            //    highlightedGridField.transform.Translate(buildposition);
+            //}
+
+            //if (Input.GetKeyDown(KeyCode.S))
+            //{
+            //    Vector3 buildposition = new Vector3(hgfPosx, hgfPosy, hgfPosz - 1);
+            //    highlightedGridField.transform.Translate(buildposition);
+            //}
+        }
+    }
+
+
+    void ChooseTower()
+    {
+        if (buildmode /*&& ui(inventory)*/)
+        {
+            /*if (D-Pad steuerung)
+            {
+                chosen = true;
+            }*/
+        }
+    }
+
+    void RotateTower()
+    {
+        if (buildmode && chosen)
+        {
+            tower = Instantiate(tower, new Vector3(transform.position.x, (transform.position.y + 2), transform.position.z), transform.rotation);
+            tower.GetComponent<Renderer>().material = outline;
+
+            if (Input.GetAxis("") >= 0/*Right Stick Steuerung -> right*/)
+            {
+                tower.transform.Rotate(0, 45, 0);
+                rotate = true;
+
+            }
+
+            if (Input.GetAxis("") <= 0/*Right Stick Steuerung -> left*/)
+            {
+                tower.transform.Rotate(0, - 45, 0);
+                rotate = true;
+            }
+        }
+    }
+
+    void PlaceTower()
+    {
+        if (Input.GetKeyDown(KeyCode.JoystickButton2) && buildmode && rotate)
+        {
+            tower.GetComponent<Renderer>().material = basematerial;
+            tower.AddComponent<Rigidbody>();
+
+            rotate = false;
+            chosen = false;
+            buildmode = false;
+
+        }
+    }
+}
