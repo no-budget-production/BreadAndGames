2bfa16a03298015597ce6e1e2ab3a4c7bb8869f7
MEDIADESIGN\feschmidt <derdenker@live.de>
2018-01-30 18:27:08 +0100

    Some Angenehmeints Änderungen

---
+++ Assets/VariableSet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableSet : MonoBehaviour
+{
+    public float xScale = 10;
+    public float zScale = 10;
+    public int intensity = 25;
+
+    public ParticleSystem[] ParticleComponents;
+
+    // Use this for initialization
+    void Start () {
+        foreach (var ParticleSystemRef in ParticleComponents)
+        {
+            ParticleSystemRef.shape.scale.Set(xScale, 1, zScale);
+            //ParticleSystemRef.emission.rateOverTime = intensity;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
