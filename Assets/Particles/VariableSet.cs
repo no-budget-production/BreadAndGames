272d621518dec2a91b6703c6d35a28a943da4fc7
Felix Schmidt <derdenker@live.de>
2018-01-31 09:33:25 +0100

    Jetzt auch mit höhen verstellung

--- Assets/Particles/VariableSet.cs
+++ Assets/Particles/VariableSet.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class VariableSet : MonoBehaviour
 {
-    public float xScale = 10;
-    public float zScale = 10;
+    public float xScale = 10f;
+    public float zScale = 10f;
+    public float height = 10f;
+    public ParticleSystemShapeType newShape = ParticleSystemShapeType.Box;
     public int intensity = 25;
 
     public ParticleSystem[] ParticleComponents;
@@ -14,7 +16,16 @@ public class VariableSet : MonoBehaviour
     void Start () {
         foreach (var ParticleSystemRef in ParticleComponents)
         {
-            ParticleSystemRef.shape.scale.Set(xScale, 1, zScale);
+            Vector3 newSize = new Vector3(xScale, 1.0f, zScale);
+
+            var shape = ParticleSystemRef.shape;
+            shape.shapeType = newShape;
+            shape.scale = newSize;
+            
+            var oldPosition = shape.position;
+            Vector3 newPosition = new Vector3(oldPosition.x, height, oldPosition.z);
+            shape.position = newPosition;
+
             var emission = ParticleSystemRef.emission;
             emission.rateOverTime = intensity;
         }
