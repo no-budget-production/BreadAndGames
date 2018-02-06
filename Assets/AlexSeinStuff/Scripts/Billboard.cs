7bbf8a918cf39d610d91b0b9947857b23016485a
derAlex <alexander.buechler25@gmail.com>
2018-02-06 12:06:49 +0100

    everything is better now

---
+++ Assets/AlexSeinStuff/Scripts/Billboard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+
+
+    void Update()
+    {
+        transform.LookAt(Camera.main.transform);
+
+    }
+}
