e5ed4313f1761f300d7acc05dad1bb020ce87833
Juri Knauth <knauth.juri@gmail.com>
2018-02-08 15:55:47 +0100

    AlexShooterAnim

--- Assets/Scripts/Skills/Skill.cs
+++ Assets/Scripts/Skills/Skill.cs
@@ -8,6 +8,7 @@ public class Skill : MonoBehaviour
     public PlayerController PlayerController;
     public GameObject[] ActivePlayers;
     public bool isFiring;
+    public Transform SkillSpawn;
 
     public virtual void Shoot()
     {
