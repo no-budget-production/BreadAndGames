f533e0fdada2a2ff626a0c33fc8a0fc91ddb34b5
MEDIADESIGN\feschmidt <derdenker@live.de>
2018-02-12 15:28:08 +0100

    ChargePunch

--- Assets/Scripts/Skills/Skills/Gun/Punch.cs
+++ Assets/Scripts/Skills/Skills/Gun/Punch.cs
@@ -9,16 +9,23 @@ public class Punch : Skill
 
     public float MsBetweenShot;
     public float Damage;
+    public float chargeTime;
+    public float cur_chargeTime;
 
     float nextShotTime;
+    void Start()
+    {
+        cur_chargeTime = chargeTime;
+    }
 
     public override void Shoot()
     {
-        if (Time.time > nextShotTime && base.PlayerController.curActionPoints > 0 && !base.PlayerController.isInAction)
+        if (cur_chargeTime < 100) cur_chargeTime -= Time.deltaTime;
+        if (Time.time > nextShotTime && base.PlayerController.curActionPoints > 0 && !base.PlayerController.isInAction && cur_chargeTime <= 0)
         {
             nextShotTime = Time.time + MsBetweenShot * 0.001f;
 
-            //Debug.Log("Punch");
+
             foreach (Character e in HitBox.Enemies)
             {
                 if (e == null)
@@ -28,6 +35,13 @@ public class Punch : Skill
                 }
                 e.TakeDamage(Damage);
             }
+
+            cur_chargeTime = 110;
         }
+    }
+
+    public override void StopShoot()
+    {
+        cur_chargeTime = chargeTime;
     }
 }
