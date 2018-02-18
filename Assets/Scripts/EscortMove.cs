4aa6d31638c762864d40324e5490484166b95ea3
Edgar Schoenknecht <edgar.schoenknecht@hotmail.de>
2018-02-18 20:22:06 +0100

    Escort Event WIP

---
+++ Assets/Scripts/EscortMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EscortMove : MonoBehaviour
+{
+    public bool startEscortEvent;
+    public Transform[] points;
+
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        startEscortEvent = false;
+    }
+
+
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+
+    void Update()
+    {
+        if (startEscortEvent)
+        {
+
+            // Choose the next destination point when the agent gets
+            // close to the current one.
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+                GotoNextPoint();
+        }
+
+
+    }
+
+}
