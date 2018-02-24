ce50afc7d141b39b44f61bd1177b97f21acd6d1f
Edgar Schoenknecht <edgar.schoenknecht@hotmail.de>
2018-02-24 22:59:22 +0100

    phase1 update

--- Assets/Scripts/ArenaSpawner.cs
+++ Assets/Scripts/ArenaSpawner.cs
@@ -5,53 +5,71 @@ using UnityEngine;
 public class ArenaSpawner : MonoBehaviour
 {
     public GameObject PrefabToSpawn;            // The prefab what should be spawned
-    
-    public float SpawnRate;                     // The time limit when the next enemy will spawn
-    public int AmountToSpawn;                   // How many enemys should spawn
 
     public Transform HoldingPoint;
 
+    public PhaseOneLevelScript PhaseOneLevelScript;
+
     private Transform EnemyHolder;
     
+    private float Timer;
 
-    private IEnumerator WaitAndSpawnCoroutine;
+    private bool StartSpawning;
+    public bool _StartSpawning { get { return StartSpawning; } set { StartSpawning = value; } }
+
+    private float Interval;
+    public float _Interval { get { return Interval; } set { Interval = value; } }
+
+    private float AmountToSpawn;
+    public float _AmountOfEnemys { get { return AmountToSpawn; } set { AmountToSpawn = value; } }
+
     private int SpawnedEnemys;
     public int _SpawnedEnemys { get { return SpawnedEnemys; } set { SpawnedEnemys = value; } }
 
-
+    void Awake()
+    {
+        Timer = 0;
+        Interval = 0;
+        AmountToSpawn = 0;
+        StartSpawning = false;
+    }
 
     void Start()
     {
-        WaitAndSpawnCoroutine = WaitAndSpawn(SpawnRate);
         EnemyHolder = GameManager.Instance.EnemyHolder;
     }
 
-    public void StartSpawn()
+    void Update()
     {
-        StartCoroutine(WaitAndSpawnCoroutine);
+        if (StartSpawning)
+        {
+            if (SpawnedEnemys < AmountToSpawn)
+            {
+                if (Timer < Interval)
+                {
+                    Timer += Time.deltaTime;
+
+                }
+                else
+                {
+                    SpawnEnemy();
+                    Timer = 0;
+                }
+
+            }
+        }
+
     }
-    void SpawnEnemy()
+
+    public void SpawnEnemy()
     {
         GameObject curPrefab;
         curPrefab = Instantiate(PrefabToSpawn, transform.position, transform.rotation) as GameObject;
-        curPrefab.transform.parent = EnemyHolder.transform;
-        
-
         SpawnedEnemys++;
+        PhaseOneLevelScript._AmountOfSpawnedEnemysInCurrentWave++;
+        curPrefab.transform.parent = EnemyHolder.transform;
 
-        if (SpawnedEnemys == AmountToSpawn)
-        {
-            StopCoroutine(WaitAndSpawnCoroutine);
-        }
     }
 
-    private IEnumerator WaitAndSpawn(float waitTime)
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(waitTime);
-            SpawnEnemy();
-        }
-    }
 }
 
