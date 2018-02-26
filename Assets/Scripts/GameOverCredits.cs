f042f1ee9b1b8b5c8d94cb2a6c2278496982b8f8
Edgar Schoenknecht <edgar.schoenknecht@hotmail.de>
2018-02-26 11:19:34 +0100

    Game Over Scene

---
+++ Assets/Scripts/GameOverCredits.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverCredits : MonoBehaviour
+{
+    public Transform creditsObj;
+    public float multi;
+
+    
+
+	void Update ()
+    {
+        creditsObj.Translate(Vector3.up * multi * Time.deltaTime);
+
+    }
+}
---
+++ Assets/Scripts/Level Scripts/GameOver.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+
+    private int playerCount;
+    public int requiredPlayerCount;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var temp = other.GetComponent<PlayerController>();
+        if (temp == null)
+            return;
+
+        playerCount++;
+        CheckPlayerCount();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var temp = other.GetComponent<PlayerController>();
+        if (temp == null)
+            return;
+
+        playerCount--;
+    }
+
+    void CheckPlayerCount()
+    {
+        if (playerCount >= requiredPlayerCount)
+        {
+
+        }
+    }
+}
