c36b067b004fefe4e25452ad186485971d7cb827
Edgar Schoenknecht <edgar.schoenknecht@hotmail.de>
2018-02-26 21:25:15 +0100

    walk sounds melee

---
+++ Assets/Scripts/MeleeAnimEvents.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAnimEvents : MonoBehaviour
+{
+
+    public AudioClip[] WalkClips;
+    public float highPitch;
+    public float lowPitch;
+
+    public AudioSource _AudioSource;
+
+    private int StepCounter;
+
+    void Start()
+    {
+        StepCounter = 1;
+    }
+
+    public void WalkSound()
+    {
+        if(StepCounter % StepCounter == 0)
+        {
+            _AudioSource.clip = WalkClips[0];
+            _AudioSource.Play();
+        }
+        else if (StepCounter % StepCounter != 0)
+        {
+            _AudioSource.clip = WalkClips[1];
+            _AudioSource.Play();
+        }
+        
+    }
+}
