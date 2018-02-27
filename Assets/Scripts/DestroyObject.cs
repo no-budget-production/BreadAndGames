49c4e20fd3be8880078f9291255ea38acc884de6
Edgar Schoenknecht <edgar.schoenknecht@hotmail.de>
2018-02-27 15:11:11 +0100

    enemy destroy explosion

---
+++ Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+
+    public float destroyTime;
+
+
+    void Start()
+    {
+        Destroy(this.gameObject, destroyTime);
+    }
+
+}
+	
+
