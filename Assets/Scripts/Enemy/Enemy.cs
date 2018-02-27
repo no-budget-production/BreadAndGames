fe78b3cd5000461601af66fc8d0bd304fe4594b8
Edgar Schoenknecht <edgar.schoenknecht@hotmail.de>
2018-02-27 19:01:19 +0100

    enemy disable

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -23,6 +23,7 @@ public class Enemy : Character
     public bool isSooting;
 
     public GameObject deadExplosionPrefab;
+    public GameObject gameOverEffect;
 
     public float MaxShootingRange;
     public float StopRange;
@@ -126,8 +127,9 @@ public class Enemy : Character
         //Anim.SetFloat(animMovX, TempSpeed + TurnSpeed.magnitude / NavMeshAgent.angularSpeed);
         _Animtor.SetFloat(animMovX, TempSpeed);
 
-        //if (!isGameOver)
+        if (!isGameOver)
         {
+
             base.Update();
 
             FrameCounterAliveCheck++;
@@ -233,6 +235,14 @@ public class Enemy : Character
                 }
 
             }
+        }
+        else
+        {
+            if (gameOverEffect != null)
+            {
+                gameOverEffect.SetActive(true);
+            }
+
         }
     }
 
