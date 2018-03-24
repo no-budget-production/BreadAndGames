c98b320de035a323b1324e112c9621c760e10765
Juri Knauth <knauth.juri@gmail.com>
2018-03-25 00:11:14 +0100

    UNITY VERSION 2017.4.0f1 - Cleaned Old Files - WIP

--- Assets/Scripts/#Placeholder/EnergyRestorationPoint.cs
+++
@@ -1,8 +0,0 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-
-//public class EnergyRestorationPoint : MonoBehaviour {
-
-//    public float EnergyMaximum;
-//}
